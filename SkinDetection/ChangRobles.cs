f5877951385e1c5699eae7f0f2d2044af2d4676b
Sanyo Gorbunov <sanyo.gorbunov@gmail.com>
2013-05-18 13:26:01 +0300

    Start MeanCovariance SkinColorModel step

---
+++ SkinDetection/ChangRobles.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkinDetection
+{
+    class TwoDimVector
+    {
+        public double a1 { get; set; }
+        public double a2 { get; set; }
+        public bool T { get; set; }
+    }
+    class TwoDimMatrix
+    {
+        public double a1 { get; set; }
+        public double a2 { get; set; }
+        public double b1 { get; set; }
+        public double b2 { get; set; }
+    }
+
+    class ChangRobles
+    {
+        public TwoDimVector Mean { get; private set; }
+        public TwoDimMatrix Covariance { get; private set; }
+
+        public void SetModel(List<double> r, List<double> b)
+        {
+            Mean = new TwoDimVector
+            {
+                T = true,
+                a1 = r.Average(),
+                a2 = b.Average()
+            };
+            
+        }
+    }
+}
