bbce812236b989444e8958a6e66e385908ae22dc
Alexander Gorbunov <sanyo.gorbunov@gmail.com>
2013-05-19 20:37:57 +0300

    Mouse Selection

--- SkinDetection/FaceColorLib.Designer.cs
+++ SkinDetection/FaceColorLib.Designer.cs
@@ -29,17 +29,32 @@
         private void InitializeComponent()
         {
             this.pnlLib = new System.Windows.Forms.Panel();
-            this.lbLib = new System.Windows.Forms.ListBox();
             this.btnLibAdd = new System.Windows.Forms.Button();
+            this.lbLib = new System.Windows.Forms.ListBox();
             this.dlgLibOpen = new System.Windows.Forms.OpenFileDialog();
             this.pnlLibType = new System.Windows.Forms.Panel();
             this.gbLibType = new System.Windows.Forms.GroupBox();
-            this.rbLibTypeManual = new System.Windows.Forms.RadioButton();
-            this.rbLibTypeAuto = new System.Windows.Forms.RadioButton();
             this.rbLibTypeFromFile = new System.Windows.Forms.RadioButton();
+            this.rbLibTypeAuto = new System.Windows.Forms.RadioButton();
+            this.rbLibTypeManual = new System.Windows.Forms.RadioButton();
+            this.pnlSelectRegion = new System.Windows.Forms.Panel();
+            this.pbFace = new System.Windows.Forms.PictureBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.pnlResults = new System.Windows.Forms.Panel();
+            this.lblRedMean = new System.Windows.Forms.Label();
+            this.lblRedMeanRes = new System.Windows.Forms.Label();
+            this.lblRedDispRes = new System.Windows.Forms.Label();
+            this.lblRedDisp = new System.Windows.Forms.Label();
+            this.lblBlueDispRes = new System.Windows.Forms.Label();
+            this.lblBlueDisp = new System.Windows.Forms.Label();
+            this.lblBlueMeanRes = new System.Windows.Forms.Label();
+            this.lblBlueMean = new System.Windows.Forms.Label();
             this.pnlLib.SuspendLayout();
             this.pnlLibType.SuspendLayout();
             this.gbLibType.SuspendLayout();
+            this.pnlSelectRegion.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFace)).BeginInit();
+            this.pnlResults.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlLib
@@ -51,14 +66,6 @@
             this.pnlLib.Size = new System.Drawing.Size(312, 265);
             this.pnlLib.TabIndex = 0;
             // 
-            // lbLib
-            // 
-            this.lbLib.FormattingEnabled = true;
-            this.lbLib.Location = new System.Drawing.Point(13, 16);
-            this.lbLib.Name = "lbLib";
-            this.lbLib.Size = new System.Drawing.Size(191, 238);
-            this.lbLib.TabIndex = 0;
-            // 
             // btnLibAdd
             // 
             this.btnLibAdd.Location = new System.Drawing.Point(210, 16);
@@ -68,6 +75,15 @@
             this.btnLibAdd.Text = "Add";
             this.btnLibAdd.UseVisualStyleBackColor = true;
             this.btnLibAdd.Click += new System.EventHandler(this.btnLibAdd_Click);
+            // 
+            // lbLib
+            // 
+            this.lbLib.FormattingEnabled = true;
+            this.lbLib.Location = new System.Drawing.Point(13, 16);
+            this.lbLib.Name = "lbLib";
+            this.lbLib.Size = new System.Drawing.Size(191, 238);
+            this.lbLib.TabIndex = 0;
+            this.lbLib.SelectedIndexChanged += new System.EventHandler(this.lbLib_SelectedIndexChanged);
             // 
             // dlgLibOpen
             // 
@@ -93,16 +109,16 @@
             this.gbLibType.TabStop = false;
             this.gbLibType.Text = "Lib Type";
             // 
-            // rbLibTypeManual
+            // rbLibTypeFromFile
             // 
-            this.rbLibTypeManual.AutoSize = true;
-            this.rbLibTypeManual.Location = new System.Drawing.Point(9, 19);
-            this.rbLibTypeManual.Name = "rbLibTypeManual";
-            this.rbLibTypeManual.Size = new System.Drawing.Size(60, 17);
-            this.rbLibTypeManual.TabIndex = 0;
-            this.rbLibTypeManual.TabStop = true;
-            this.rbLibTypeManual.Text = "Manual";
-            this.rbLibTypeManual.UseVisualStyleBackColor = true;
+            this.rbLibTypeFromFile.AutoSize = true;
+            this.rbLibTypeFromFile.Location = new System.Drawing.Point(128, 19);
+            this.rbLibTypeFromFile.Name = "rbLibTypeFromFile";
+            this.rbLibTypeFromFile.Size = new System.Drawing.Size(67, 17);
+            this.rbLibTypeFromFile.TabIndex = 3;
+            this.rbLibTypeFromFile.TabStop = true;
+            this.rbLibTypeFromFile.Text = "From File";
+            this.rbLibTypeFromFile.UseVisualStyleBackColor = true;
             // 
             // rbLibTypeAuto
             // 
@@ -115,30 +131,154 @@
             this.rbLibTypeAuto.Text = "Auto";
             this.rbLibTypeAuto.UseVisualStyleBackColor = true;
             // 
-            // rbLibTypeFromFile
+            // rbLibTypeManual
             // 
-            this.rbLibTypeFromFile.AutoSize = true;
-            this.rbLibTypeFromFile.Location = new System.Drawing.Point(128, 19);
-            this.rbLibTypeFromFile.Name = "rbLibTypeFromFile";
-            this.rbLibTypeFromFile.Size = new System.Drawing.Size(67, 17);
-            this.rbLibTypeFromFile.TabIndex = 3;
-            this.rbLibTypeFromFile.TabStop = true;
-            this.rbLibTypeFromFile.Text = "From File";
-            this.rbLibTypeFromFile.UseVisualStyleBackColor = true;
+            this.rbLibTypeManual.AutoSize = true;
+            this.rbLibTypeManual.Location = new System.Drawing.Point(9, 19);
+            this.rbLibTypeManual.Name = "rbLibTypeManual";
+            this.rbLibTypeManual.Size = new System.Drawing.Size(60, 17);
+            this.rbLibTypeManual.TabIndex = 0;
+            this.rbLibTypeManual.TabStop = true;
+            this.rbLibTypeManual.Text = "Manual";
+            this.rbLibTypeManual.UseVisualStyleBackColor = true;
+            // 
+            // pnlSelectRegion
+            // 
+            this.pnlSelectRegion.Controls.Add(this.pbFace);
+            this.pnlSelectRegion.Location = new System.Drawing.Point(330, 109);
+            this.pnlSelectRegion.Name = "pnlSelectRegion";
+            this.pnlSelectRegion.Size = new System.Drawing.Size(299, 265);
+            this.pnlSelectRegion.TabIndex = 2;
+            // 
+            // pbFace
+            // 
+            this.pbFace.Location = new System.Drawing.Point(3, 3);
+            this.pbFace.Name = "pbFace";
+            this.pbFace.Size = new System.Drawing.Size(167, 189);
+            this.pbFace.TabIndex = 0;
+            this.pbFace.TabStop = false;
+            this.pbFace.Paint += new System.Windows.Forms.PaintEventHandler(this.pbFace_Paint);
+            this.pbFace.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbFace_MouseDown);
+            this.pbFace.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbFace_MouseMove);
+            this.pbFace.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbFace_MouseUp);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(682, 12);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(171, 43);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // pnlResults
+            // 
+            this.pnlResults.Controls.Add(this.lblBlueDispRes);
+            this.pnlResults.Controls.Add(this.lblBlueDisp);
+            this.pnlResults.Controls.Add(this.lblBlueMeanRes);
+            this.pnlResults.Controls.Add(this.lblBlueMean);
+            this.pnlResults.Controls.Add(this.lblRedDispRes);
+            this.pnlResults.Controls.Add(this.lblRedDisp);
+            this.pnlResults.Controls.Add(this.lblRedMeanRes);
+            this.pnlResults.Controls.Add(this.lblRedMean);
+            this.pnlResults.Location = new System.Drawing.Point(682, 61);
+            this.pnlResults.Name = "pnlResults";
+            this.pnlResults.Size = new System.Drawing.Size(171, 135);
+            this.pnlResults.TabIndex = 4;
+            // 
+            // lblRedMean
+            // 
+            this.lblRedMean.AutoSize = true;
+            this.lblRedMean.Location = new System.Drawing.Point(3, 10);
+            this.lblRedMean.Name = "lblRedMean";
+            this.lblRedMean.Size = new System.Drawing.Size(107, 13);
+            this.lblRedMean.TabIndex = 0;
+            this.lblRedMean.Text = "Chromatic Red Mean";
+            // 
+            // lblRedMeanRes
+            // 
+            this.lblRedMeanRes.AutoSize = true;
+            this.lblRedMeanRes.Location = new System.Drawing.Point(116, 10);
+            this.lblRedMeanRes.Name = "lblRedMeanRes";
+            this.lblRedMeanRes.Size = new System.Drawing.Size(35, 13);
+            this.lblRedMeanRes.TabIndex = 1;
+            this.lblRedMeanRes.Text = "label1";
+            // 
+            // lblRedDispRes
+            // 
+            this.lblRedDispRes.AutoSize = true;
+            this.lblRedDispRes.Location = new System.Drawing.Point(116, 35);
+            this.lblRedDispRes.Name = "lblRedDispRes";
+            this.lblRedDispRes.Size = new System.Drawing.Size(35, 13);
+            this.lblRedDispRes.TabIndex = 3;
+            this.lblRedDispRes.Text = "label1";
+            // 
+            // lblRedDisp
+            // 
+            this.lblRedDisp.AutoSize = true;
+            this.lblRedDisp.Location = new System.Drawing.Point(3, 35);
+            this.lblRedDisp.Name = "lblRedDisp";
+            this.lblRedDisp.Size = new System.Drawing.Size(101, 13);
+            this.lblRedDisp.TabIndex = 2;
+            this.lblRedDisp.Text = "Chromatic Red Disp";
+            // 
+            // lblBlueDispRes
+            // 
+            this.lblBlueDispRes.AutoSize = true;
+            this.lblBlueDispRes.Location = new System.Drawing.Point(116, 94);
+            this.lblBlueDispRes.Name = "lblBlueDispRes";
+            this.lblBlueDispRes.Size = new System.Drawing.Size(35, 13);
+            this.lblBlueDispRes.TabIndex = 7;
+            this.lblBlueDispRes.Text = "label1";
+            // 
+            // lblBlueDisp
+            // 
+            this.lblBlueDisp.AutoSize = true;
+            this.lblBlueDisp.Location = new System.Drawing.Point(3, 94);
+            this.lblBlueDisp.Name = "lblBlueDisp";
+            this.lblBlueDisp.Size = new System.Drawing.Size(102, 13);
+            this.lblBlueDisp.TabIndex = 6;
+            this.lblBlueDisp.Text = "Chromatic Blue Disp";
+            // 
+            // lblBlueMeanRes
+            // 
+            this.lblBlueMeanRes.AutoSize = true;
+            this.lblBlueMeanRes.Location = new System.Drawing.Point(116, 69);
+            this.lblBlueMeanRes.Name = "lblBlueMeanRes";
+            this.lblBlueMeanRes.Size = new System.Drawing.Size(35, 13);
+            this.lblBlueMeanRes.TabIndex = 5;
+            this.lblBlueMeanRes.Text = "label1";
+            // 
+            // lblBlueMean
+            // 
+            this.lblBlueMean.AutoSize = true;
+            this.lblBlueMean.Location = new System.Drawing.Point(3, 69);
+            this.lblBlueMean.Name = "lblBlueMean";
+            this.lblBlueMean.Size = new System.Drawing.Size(108, 13);
+            this.lblBlueMean.TabIndex = 4;
+            this.lblBlueMean.Text = "Chromatic Blue Mean";
             // 
             // FaceColorLib
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(679, 409);
+            this.ClientSize = new System.Drawing.Size(889, 409);
+            this.Controls.Add(this.pnlResults);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.pnlSelectRegion);
             this.Controls.Add(this.pnlLibType);
             this.Controls.Add(this.pnlLib);
             this.Name = "FaceColorLib";
-            this.Text = "Form1";
+            this.Text = "Face Color Library";
             this.pnlLib.ResumeLayout(false);
             this.pnlLibType.ResumeLayout(false);
             this.gbLibType.ResumeLayout(false);
             this.gbLibType.PerformLayout();
+            this.pnlSelectRegion.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbFace)).EndInit();
+            this.pnlResults.ResumeLayout(false);
+            this.pnlResults.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -154,6 +294,18 @@
         private System.Windows.Forms.RadioButton rbLibTypeFromFile;
         private System.Windows.Forms.RadioButton rbLibTypeAuto;
         private System.Windows.Forms.RadioButton rbLibTypeManual;
+        private System.Windows.Forms.Panel pnlSelectRegion;
+        private System.Windows.Forms.PictureBox pbFace;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Panel pnlResults;
+        private System.Windows.Forms.Label lblBlueDispRes;
+        private System.Windows.Forms.Label lblBlueDisp;
+        private System.Windows.Forms.Label lblBlueMeanRes;
+        private System.Windows.Forms.Label lblBlueMean;
+        private System.Windows.Forms.Label lblRedDispRes;
+        private System.Windows.Forms.Label lblRedDisp;
+        private System.Windows.Forms.Label lblRedMeanRes;
+        private System.Windows.Forms.Label lblRedMean;
 
     }
 }
--- SkinDetection/FaceColorLib.cs
+++ SkinDetection/FaceColorLib.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -23,8 +24,116 @@ namespace SkinDetection
         {
             if (dlgLibOpen.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-
+                lbLib.Items.Add(dlgLibOpen.FileName);
             }
+        }
+
+        #region Select Area on pbFace
+
+        private Point RectStartPoint;
+        private Rectangle Rect = new Rectangle();
+        private Brush selectionBrush = new SolidBrush(Color.FromArgb(128, 72, 145, 220));
+
+        private void pbFace_Paint(object sender, PaintEventArgs e)
+        {
+            if (pbFace.Image != null)
+            {
+                if (Rect != null && Rect.Width > 0 && Rect.Height > 0)
+                {
+                    e.Graphics.FillRectangle(selectionBrush, Rect);
+                }
+            }
+        }
+
+        private void pbFace_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                if (Rect.Contains(e.Location))
+                {
+
+                }
+            }
+        }
+
+        private void pbFace_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button != System.Windows.Forms.MouseButtons.Left)
+            {
+                return;
+            }
+            Point tempEndPoint = e.Location;
+            Rect.Location = new Point(
+                Math.Min(RectStartPoint.X, tempEndPoint.X),
+                Math.Min(RectStartPoint.Y, tempEndPoint.Y)
+                );
+            Rect.Size = new Size(
+                Math.Abs(RectStartPoint.X - tempEndPoint.X),
+                Math.Abs(RectStartPoint.Y - tempEndPoint.Y)
+                );
+            pbFace.Invalidate();
+        }
+
+        private void pbFace_MouseDown(object sender, MouseEventArgs e)
+        {
+            RectStartPoint = e.Location;
+            Invalidate();
+        }
+
+        private void lbLib_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string path = lbLib.SelectedItem.ToString();
+            if (File.Exists(path))
+            {
+                pbFace.Image = new Bitmap(path);
+            }
+        }
+
+        #endregion
+
+        private double crMean, cbMean, crD, cbD;
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            List<double> cr = new List<double>();
+            List<double> cb = new List<double>();
+            foreach (var item in lbLib.Items)
+            {
+                Image<Bgr, Byte> img = new Image<Bgr, byte>(item.ToString());
+                for (int i = Rect.Top; i < Rect.Top + Rect.Height; i++)
+                {
+                    for (int j = Rect.Left; j < Rect.Left + Rect.Width; j++)
+                    {
+                        double s = img[i, j].Red + img[i, j].Green + img[i, j].Blue;
+                        double r = img[i, j].Red / s;
+                        double b = img[i, j].Blue / s;
+                        cr.Add(r);
+                        cb.Add(b);
+                    }
+                }
+            }
+            CalcMeanAndDisp(cr, cb);
+            PrintMeanAndDisp();
+        }
+        private void PrintMeanAndDisp()
+        {
+            lblRedMeanRes.Text = crMean.ToString("0.0000");
+            lblRedDispRes.Text = crD.ToString("0.0000");
+            lblBlueMeanRes.Text = crMean.ToString("0.0000");
+            lblBlueDispRes.Text = cbD.ToString("0.0000");
+        }
+        private void CalcMeanAndDisp(List<double> cr, List<double> cb)
+        {
+            crMean = cr.Average();
+            cbMean = cr.Average();
+            crD = 0; cbD = 0;
+            for (int i = 0; i < cr.Count; i++)
+            {
+                crD += (cr[i] - crMean) * (cr[i] - crMean);
+                cbD += (cb[i] - cbMean) * (cb[i] - cbMean);
+            }
+            crD /= (cr.Count - 1);
+            cbD /= (cb.Count - 1);
         }
     }
 }
