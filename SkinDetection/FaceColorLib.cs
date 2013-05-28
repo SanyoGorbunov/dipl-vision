28048af13aa672e804238bb146b14825dc98ab00
Alexander Gorbunov <sanyo.gorbunov@gmail.com>
2013-05-29 02:30:47 +0300

    Divide to regions the main form.

--- SkinDetection/FaceColorLib.cs
+++ SkinDetection/FaceColorLib.cs
@@ -15,18 +15,15 @@ namespace SkinDetection
 {
     public partial class FaceColorLib : Form
     {
+        private ChangRobles model = new ChangRobles();
+
+        private Image<Bgr, Byte> imgTest;
+        private Image<Gray, Byte> imgLikelihood;
+        private Image<Gray, Byte> imgBinary;
+
         public FaceColorLib()
         {
             InitializeComponent();
-        }
-
-        private void btnLibAdd_Click(object sender, EventArgs e)
-        {
-            if (dlgLibOpen.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                lbLib.Items.Add(dlgLibOpen.FileName);
-                personRects.Add(new List<Rectangle>());
-            }
         }
 
         #region Select Area on pbFace
@@ -120,139 +117,15 @@ namespace SkinDetection
 
         #endregion
 
-        private ChangRobles model = new ChangRobles();
+        #region Lib manipulations
 
-        private void btnCalculate_Click(object sender, EventArgs e)
+        private void btnLibAdd_Click(object sender, EventArgs e)
         {
-            List<double> cr = new List<double>();
-            List<double> cb = new List<double>();
-            for (int k = 0; k < lbLib.Items.Count; k++)
+            if (dlgLibOpen.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                Image<Bgr, Byte> img = new Image<Bgr, byte>(lbLib.Items[k].ToString());
-                foreach (var Rect in personRects[k])
-                {
-                    for (int i = Rect.Top; i < Rect.Top + Rect.Height; i++)
-                    {
-                        for (int j = Rect.Left; j < Rect.Left + Rect.Width; j++)
-                        {
-                            double s = img[i, j].Red + img[i, j].Green + img[i, j].Blue;
-                            double r = img[i, j].Red / s;
-                            double b = img[i, j].Blue / s;
-                            cr.Add(r);
-                            cb.Add(b);
-                        }
-                    }   
-                }
+                lbLib.Items.Add(dlgLibOpen.FileName);
+                personRects.Add(new List<Rectangle>());
             }
-            model.SetModel(cr, cb);
-            PrintColorModel();
-        }
-
-        private void PrintColorModel()
-        {
-            lblMean.Text = string.Format("{0:0.000000} {1:0.000000}", model.Mean.a1, model.Mean.a2);
-            lblCov1.Text = string.Format("{0:0.000000} {1:0.000000}", model.Covariance.a, model.Covariance.b);
-            lblCov2.Text = string.Format("{0:0.000000} {1:0.000000}", model.Covariance.c, model.Covariance.d);
-        }
-
-        private void btnSaveColorModel_Click(object sender, EventArgs e)
-        {
-            if (dlgSaveColorModel.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                model.Save(dlgSaveColorModel.FileName);
-            }
-        }
-
-        private void btnLoadColorModel_Click(object sender, EventArgs e)
-        {
-            if (dlgLoadColorModel.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                model = ChangRobles.Load(dlgLoadColorModel.FileName);
-                PrintColorModel();
-            }
-        }
-
-        private Image<Bgr, Byte> imgTest;
-        private Image<Gray, Byte> imgLikelihood;
-        private Image<Gray, Byte> imgBinary;
-        private void btnLoadTestImage_Click(object sender, EventArgs e)
-        {
-            if (dlgLoadTestImage.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                imgTest = new Image<Bgr, byte>(dlgLoadTestImage.FileName);
-                pbFace.Image = imgTest.ToBitmap();
-            }
-        }
-
-        private void btnDisplayLikelihood_Click(object sender, EventArgs e)
-        {
-            imgLikelihood = model.GetLikelihoodImage(imgTest);
-            pbFace.Image = imgLikelihood.ToBitmap();
-        }
-
-        private void btnSetTreshold_Click(object sender, EventArgs e)
-        {
-            byte bound = byte.Parse(txtTreshBound.Text);
-            imgBinary = model.GetBinaryThreshold(imgLikelihood, bound);
-            pbFace.Image = imgBinary.ToBitmap();
-        }
-
-        private void btnRunThresh_Click(object sender, EventArgs e)
-        {
-            imgBinary = model.GetAdaptiveBinaryThreshold(imgLikelihood);
-            pbFace.Image = imgBinary.ToBitmap();
-        }
-
-        private List<SkinRegion> skinRegions;
-        private void btnFindSkinRegions_Click(object sender, EventArgs e)
-        {
-            skinRegions = model.GetSkinRegions(imgBinary);
-            FillSkinRegions();
-        }
-        private void FillSkinRegions()
-        {
-            lbSkinRegions.Items.Clear();
-            for (int i = 0; i < skinRegions.Count; i++)
-            {
-                lbSkinRegions.Items.Add("SkinRegion #" + i);
-            }
-        }
-
-        private void lbSkinRegions_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            int selectedIndex = lbSkinRegions.SelectedIndex;
-            if (skinRegions != null && selectedIndex >= 0 && selectedIndex < skinRegions.Count)
-            {
-                pbFace.Image = skinRegions[selectedIndex].Region.ToBitmap();
-
-                lblSkinRegionPixels.Text = string.Format("Skin Region has {0} pixels.", skinRegions[selectedIndex].Pixels);
-                lblSkinRegionLeft.Text = string.Format("Skin Region Left is {0}.", skinRegions[selectedIndex].Left);
-                lblSkinRegionWidth.Text = string.Format("Skin Region Width is {0}.", skinRegions[selectedIndex].Width);
-                lblSkinRegionTop.Text = string.Format("Skin Region Top is {0}.", skinRegions[selectedIndex].Top);
-                lblSkinRegionHeight.Text = string.Format("Skin Region Height is {0}.", skinRegions[selectedIndex].Height);
-                lblSkinRegionHoles.Text = string.Format("Skin Region has {0} holes.", skinRegions[selectedIndex].Holes);
-                lblSkinRegionCentroid.Text = string.Format("Skin Region centroid is ({0},{1}).", skinRegions[selectedIndex].Centroid.X, skinRegions[selectedIndex].Centroid.Y);
-                lblSkinRegionInclination.Text = string.Format("Skin Region inclination is {0}.", skinRegions[selectedIndex].Inclination);
-                lblSkinRegionRatio.Text = string.Format("Skin Region ratio is {0}.", skinRegions[selectedIndex].Ratio);
-            }
-        }
-
-        private void btnFilterByHoles_Click(object sender, EventArgs e)
-        {
-            skinRegions = model.FilterSkinRegionsByHoles(skinRegions);
-            FillSkinRegions();
-        }
-
-        private void btnApplyKernel_Click(object sender, EventArgs e)
-        {
-            float[,] k = new float[,] {
-                {1f/9, 1f/9, 1f,9},
-                {1f/9, 1f/9, 1f,9},
-                {1f/9, 1f/9, 1f,9}
-            };
-
-            imgTest = Utils.ApplyKernel(imgTest, k);
-            pbFace.Image = imgTest.ToBitmap();
         }
 
         private void btnSaveLib_Click(object sender, EventArgs e)
@@ -306,5 +179,154 @@ namespace SkinDetection
                 }
             }
         }
+
+        #endregion
+
+        #region Color model
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            List<double> cr = new List<double>();
+            List<double> cb = new List<double>();
+            for (int k = 0; k < lbLib.Items.Count; k++)
+            {
+                Image<Bgr, Byte> img = new Image<Bgr, byte>(lbLib.Items[k].ToString());
+                foreach (var Rect in personRects[k])
+                {
+                    for (int i = Rect.Top; i < Rect.Top + Rect.Height; i++)
+                    {
+                        for (int j = Rect.Left; j < Rect.Left + Rect.Width; j++)
+                        {
+                            double s = img[i, j].Red + img[i, j].Green + img[i, j].Blue;
+                            double r = img[i, j].Red / s;
+                            double b = img[i, j].Blue / s;
+                            cr.Add(r);
+                            cb.Add(b);
+                        }
+                    }   
+                }
+            }
+            model.SetModel(cr, cb);
+            PrintColorModel();
+        }
+
+        private void PrintColorModel()
+        {
+            lblMean.Text = string.Format("{0:0.000000} {1:0.000000}", model.Mean.a1, model.Mean.a2);
+            lblCov1.Text = string.Format("{0:0.000000} {1:0.000000}", model.Covariance.a, model.Covariance.b);
+            lblCov2.Text = string.Format("{0:0.000000} {1:0.000000}", model.Covariance.c, model.Covariance.d);
+        }
+
+        private void btnSaveColorModel_Click(object sender, EventArgs e)
+        {
+            if (dlgSaveColorModel.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                model.Save(dlgSaveColorModel.FileName);
+            }
+        }
+
+        private void btnLoadColorModel_Click(object sender, EventArgs e)
+        {
+            if (dlgLoadColorModel.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                model = ChangRobles.Load(dlgLoadColorModel.FileName);
+                PrintColorModel();
+            }
+        }
+
+        #endregion
+
+        #region Load test image
+
+        private void btnLoadTestImage_Click(object sender, EventArgs e)
+        {
+            if (dlgLoadTestImage.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                imgTest = new Image<Bgr, byte>(dlgLoadTestImage.FileName);
+                pbFace.Image = imgTest.ToBitmap();
+            }
+        }
+
+        private void btnApplyKernel_Click(object sender, EventArgs e)
+        {
+            float[,] k = new float[,] {
+                {1f/9, 1f/9, 1f,9},
+                {1f/9, 1f/9, 1f,9},
+                {1f/9, 1f/9, 1f,9}
+            };
+
+            imgTest = Utils.ApplyKernel(imgTest, k);
+            pbFace.Image = imgTest.ToBitmap();
+        }
+
+        #endregion
+
+        #region Thresholding
+
+        private void btnDisplayLikelihood_Click(object sender, EventArgs e)
+        {
+            imgLikelihood = model.GetLikelihoodImage(imgTest);
+            pbFace.Image = imgLikelihood.ToBitmap();
+        }
+
+        private void btnSetTreshold_Click(object sender, EventArgs e)
+        {
+            byte bound = byte.Parse(txtTreshBound.Text);
+            imgBinary = model.GetBinaryThreshold(imgLikelihood, bound);
+            pbFace.Image = imgBinary.ToBitmap();
+        }
+
+        private void btnRunThresh_Click(object sender, EventArgs e)
+        {
+            imgBinary = model.GetAdaptiveBinaryThreshold(imgLikelihood);
+            pbFace.Image = imgBinary.ToBitmap();
+        }
+
+        #endregion
+
+        #region Skin Regions
+
+        private List<SkinRegion> skinRegions;
+        private void btnFindSkinRegions_Click(object sender, EventArgs e)
+        {
+            skinRegions = model.GetSkinRegions(imgBinary);
+            FillSkinRegions();
+        }
+        private void FillSkinRegions()
+        {
+            lbSkinRegions.Items.Clear();
+            for (int i = 0; i < skinRegions.Count; i++)
+            {
+                lbSkinRegions.Items.Add("SkinRegion #" + i);
+            }
+        }
+
+        private void lbSkinRegions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex = lbSkinRegions.SelectedIndex;
+            if (skinRegions != null && selectedIndex >= 0 && selectedIndex < skinRegions.Count)
+            {
+                pbFace.Image = skinRegions[selectedIndex].Region.ToBitmap();
+
+                lblSkinRegionPixels.Text = string.Format("Skin Region has {0} pixels.", skinRegions[selectedIndex].Pixels);
+                lblSkinRegionLeft.Text = string.Format("Skin Region Left is {0}.", skinRegions[selectedIndex].Left);
+                lblSkinRegionWidth.Text = string.Format("Skin Region Width is {0}.", skinRegions[selectedIndex].Width);
+                lblSkinRegionTop.Text = string.Format("Skin Region Top is {0}.", skinRegions[selectedIndex].Top);
+                lblSkinRegionHeight.Text = string.Format("Skin Region Height is {0}.", skinRegions[selectedIndex].Height);
+                lblSkinRegionHoles.Text = string.Format("Skin Region has {0} holes.", skinRegions[selectedIndex].Holes);
+                lblSkinRegionCentroid.Text = string.Format("Skin Region centroid is ({0},{1}).", skinRegions[selectedIndex].Centroid.X, skinRegions[selectedIndex].Centroid.Y);
+                lblSkinRegionInclination.Text = string.Format("Skin Region inclination is {0}.", skinRegions[selectedIndex].Inclination);
+                lblSkinRegionRatio.Text = string.Format("Skin Region ratio is {0}.", skinRegions[selectedIndex].Ratio);
+            }
+        }
+
+        private void btnFilterByHoles_Click(object sender, EventArgs e)
+        {
+            skinRegions = model.FilterSkinRegionsByHoles(skinRegions);
+            FillSkinRegions();
+        }
+
+        #endregion
+
     }
 }
